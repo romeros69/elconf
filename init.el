7726b7cd6c49bfcc7577d400b6570c02ac4ebbad
romeros69 <jefopc@gmail.com>
2025-02-15 16:54:30 +0300

    create init.el
    Signed-off-by: romeros69 <jefopc@gmail.com>

---
+++ init.el
@@ -0,0 +1,14 @@
+;;; global settings
+
+;; line numbering
+(global-display-line-numbers-mode t)
+
+;; autosave files
+(setq auto-save-default t)
+
+;; disabling audio signals
+(setq visible-bell t)
+
+;; fullscreen emacs when start
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
